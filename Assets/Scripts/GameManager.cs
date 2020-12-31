15b0b15b804a921a0df47d732004a650fce72b10
Damien Harwood <1019243@student.sae.edu.au>
2021-01-01 00:30:41 +1100

    Added some stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+
+
+    // instance
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void AddScore(int scoreToGive)
+    {
+        
+    }
+
+    public void LevelEnd()
+    {
+        if (SceneManager.sceneCountInBuildSettings == SceneManager.GetActiveScene().buildIndex + 1)
+        {
+            WinGame();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    public void WinGame()
+    {
+
+    }
+
+    public void GameOver()
+    {
+    }
+}
