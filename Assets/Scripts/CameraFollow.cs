a8441147f83f83fd9363a60b26733eab4b6d71f3
Damien Harwood <1019243@student.sae.edu.au>
2021-01-02 19:56:56 +1100

    Edited the camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,22 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPosition;
+        
+       transform.LookAt(target);
+    }
 
     void Update()
     {
-        if (target == null)
-        {
-            Debug.Log("The Camera doesn't have a target set");
-            return;
-        }
-
-        Vector3 newPos = target.position + offset;
-        newPos.y = offset.y;
-        transform.position = newPos;
     }
 }
