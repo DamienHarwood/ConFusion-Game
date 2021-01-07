89c23208836d4536a416095a90801285e080730c
Damien Harwood <1019243@student.sae.edu.au>
2021-01-07 16:10:35 +1100

    Movement work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,61 +13,57 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
-    private Rigidbody rig;
-
+    public Rigidbody rb;
     private float playerScore;
-    Renderer render;
     public float raycastSize = 0.5f;
 
     public GameManager gameManager;
 
+    public string keyA = "KeyCode.A";
     void Awake()
     {
         // get the rigidbody component
-        rig = GetComponent<Rigidbody>();
-        render = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-       NoMixMove(); //Uncomment/comment when Movement needs to not be mixed around - useful for testing features.
-                    //Currently works well 02/01/21
-                    
-      // MixedMove(); //Uncomment/comment when Movement needs to be shuffled - this is how the controls should be for the game
-                    //Currently does not work 02/01/21
+        //NoMixMove(); //Uncomment/comment when Movement needs to not be mixed around - useful for testing features.
+        //Currently works well 02/01/21
+
+        MixedMove(); //Uncomment/comment when Movement needs to be shuffled - this is how the controls should be for the game
+        //Currently does not work 02/01/21
     }
 
     void MixedMove()
     {
-        if (Input.GetKey("a"))
+        /*
+         * 
+         *-x = left
+         * x = right
+         *-z = down
+         * z = up
+         * 
+         */
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back);
+
+
+       // if (Input.GetKey("d")) //right
         {
-            Vector3 dir = new Vector3(-1, 0, 0) * moveSpeed;
-            dir.y = rig.velocity.y;
-            rig.velocity = dir;
-        }
-        
-        if (Input.GetKey("w"))
-        {
-            Vector3 dir = new Vector3(0, 0, 1) * moveSpeed;
-            dir.y = rig.velocity.y;
-            rig.velocity = dir;
-        }
-        
-        if (Input.GetKey("s"))
-        {
-            Vector3 dir = new Vector3(0, 0, -1) * moveSpeed;
-            dir.y = rig.velocity.y;
-            rig.velocity = dir;
-        }
-        
-        if (Input.GetKey("d"))
-        {
-            Vector3 dir = new Vector3(1, 0, 0) * moveSpeed;
-            dir.y = rig.velocity.y;
-            rig.velocity = dir;
+          //  Vector3 dir = new Vector3(1, 0, 0) * moveSpeed;
+           // dir.y = rb.velocity.y;
+          //  rb.velocity = dir;
         }
     }
-    
+
     void NoMixMove()
     {
         float xInput = Input.GetAxis("Horizontal");
@@ -75,9 +71,9 @@ public class PlayerController : MonoBehaviour
 
 
         Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
-        dir.y = rig.velocity.y;
+        dir.y = rb.velocity.y;
 
-        rig.velocity = dir;
+        rb.velocity = dir;
     }
 
 
